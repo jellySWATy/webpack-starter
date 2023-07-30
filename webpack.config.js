62107acf46679422abaa087305d6147db1da588e
Ilia Baronenkov <ilyabaronenkov@gmail.com>
2023-07-30 17:58:42 +0300

    Added basic support of TypeScript and React

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["asset/resource"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
