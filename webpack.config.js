85984437192af4147e1476ffd59402a348fd4063
Ilia Baronenkov <ilyabaronenkov@gmail.com>
2023-07-31 15:34:25 +0300

    added aliases

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
   },
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   devServer: {
     static: path.join(__dirname, "dist"),
